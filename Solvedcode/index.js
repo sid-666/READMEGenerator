ea4751a1ff3e2c15cbe44dd8391d1d71f4e1e53e
Seance <siddharth.dhananjay@gmail.com>
2020-10-10 17:14:57 +1100

    stored questions in question array, with type, message and name

--- Solvedcode/index.js
+++ Solvedcode/index.js
@@ -2,4 +2,37 @@ const inquirer = require("inquirer");
 const fs = require("fs")
 const util = require("util")
 const writetofileAsync = util.promisify(fs.writeFile)
+// array of questions for user
+const questions = [
+    {
+        type: "input",
+        message: "What is the project title?",
+        name: "title"
+    },
+    {
+        type: "input",
+        message: "Describe it?",
+        name: "description"
+    },
+    {
+        type: "input",
+        message: "insatallation method:",
+        name: "installation"
+    },
+    {
+        type: "input",
+        message: "Use case of app:",
+        name: "usage"
+    },
+    {
+        type: "input",
+        message: "Credit:",
+        name: "credit"
+    },
+    {
+        type: "input",
+        message: "License:",
+        name: "license"
+    },
+];
 
